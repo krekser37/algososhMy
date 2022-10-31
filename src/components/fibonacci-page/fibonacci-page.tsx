742e5f9f450153f0e6ec050877ca7706713a44b6
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-10-31 23:01:33 +0300

    develop

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <section className="section">
+        <Input maxLength={19} /* value={inputValue} onChange={onChangeValue} */ />
+        <Button
+          text="Развернуть"
+          linkedList={"small"}
+/*           onClick={onClick}
+          isLoader={loader}
+          disabled={!inputValue} */
+        />
+        <p className="stringSubtitle">Максимум — 19 символов</p>
+      </section>
     </SolutionLayout>
   );
 };
