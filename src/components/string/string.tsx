bd2bac207c61b5f254c69297b4c71dc4e9e4b050
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-10-31 22:53:17 +0300

    fix:string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -6,10 +6,9 @@ import { Circle } from "../ui/circle/circle";
 import "./string.css";
 import { ElementStates } from "../../types/element-states";
 import { DELAY_IN_MS } from "../../constants/delays";
-import { delay } from "../../utils/utils";
+import { delay, swap } from "../../utils/utils";
 
-
-type TArray = {
+export type TArray = {
   value: string;
   color: ElementStates;
 };
@@ -17,7 +16,7 @@ type TArray = {
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = useState("");
   const [reverseInputValue, setReverseInputValue] = useState<Array<TArray>>([]);
-  const [loader, setLoader]= useState(false);
+  const [loader, setLoader] = useState(false);
 
   const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -27,28 +26,25 @@ export const StringComponent: React.FC = () => {
     const array = inputValue
       .split("")
       .map((value) => ({ value, color: ElementStates.Default }));
-    console.log(typeof array);
     reverse(array);
   };
 
-  const reverse = async (array:TArray[]) => {
+  const reverse = async (array: TArray[]) => {
     setLoader(true);
     const mid = Math.ceil(array.length / 2);
-    console.log(array);
-
-    console.log(array.length);
-    console.log(mid);
-    console.log(array[mid]);
-    for (let i=0; i <= mid; i++) {
-      let j = array.length -1-i;
-/*       console.log(array[array.length-1]); */
-      if (i !==mid /* && i!==mid */) {
+    for (let i = 0; i < mid; i++) {
+      let j = array.length - 1 - i;
+      if (array.length === 1) {
+        array[i].color = ElementStates.Modified;
+      } else if (i < j) {
         array[i].color = ElementStates.Changing;
         array[j].color = ElementStates.Changing;
+/*         console.log("array[i]=", array[i], "array[j]=", array[j]); */
         setReverseInputValue([...array]);
+        swap(array, i, j);
         await delay(DELAY_IN_MS);
-      }
-      swap(array, i, j);
+      } 
+/*       console.log("array[i]=", array[i], "array[j]=", array[j]); */
       array[i].color = ElementStates.Modified;
       array[j].color = ElementStates.Modified;
       setReverseInputValue([...array]);
@@ -56,17 +52,17 @@ export const StringComponent: React.FC = () => {
     setLoader(false);
   };
 
-  const swap = (arr: TArray[], firstIndex: number, secondIndex: number): void => {
-    const temp = arr[firstIndex];
-    arr[firstIndex] = arr[secondIndex];
-    arr[secondIndex] = temp;
-  };
-
   return (
     <SolutionLayout title="Строка">
       <section className="section">
         <Input maxLength={11} value={inputValue} onChange={onChangeValue} />
-        <Button text="Развернуть" linkedList={"small"} onClick={onClick} isLoader={loader} disabled={!inputValue}/>
+        <Button
+          text="Развернуть"
+          linkedList={"small"}
+          onClick={onClick}
+          isLoader={loader}
+          disabled={!inputValue}
+        />
         <p className="stringSubtitle">Максимум — 11 символов</p>
       </section>
       <ul className="list">
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,11 @@
+import { TArray } from "../components/string/string";
+
 export const delay = (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+export const swap = (arr: TArray[], firstIndex: number, secondIndex: number): void => {
+    const temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
 };
